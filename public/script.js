8496cbd924eb3395e0ad9adbd271ec864fcc40e2
Fabian Tam <fabian.tam@linecorp.com>
2021-05-21 22:41:57 +0900

    Make the form send the POST request successfully
    Also send the request through the same host

--- public/script.js
+++ public/script.js
@@ -1,28 +1,49 @@
 var formdata = new FormData();
 
-const baseUrl = "https://us-central1-secret-messages-7749d.cloudfunctions.net/"; //create?secret=hi&limit=1"
+// const baseUrl = "https://us-central1-secret-messages-7749d.cloudfunctions.net/"; //create?secret=hi&limit=1"
 
 
-const postRequestOptions = {
-  method: 'POST',
-  redirect: 'follow'
-};
+
+
+const createForm = document.getElementById("create-form");
+createForm.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+	event.preventDefault();
+	const form = event.currentTarget;
+	const formData = Object.fromEntries(new FormData(form).entries());
+
+	createSecret(
+		formData.secret, 
+		formData.limit, 
+		formData.expiry, 
+		formData.expiryUnit, 
+		formData.passphrase
+	);
+}
 
 
 function createSecret(secret, accessesLimit, expiry, expiryUnit, passphrase) {
-	// const encrypted = encrypt(secret, passphrase);
-	// const params = {
-	// 	"secret": encrypted,
-	// 	"limit": accessesLimit,
-	// 	"expiry": expiry
-	// };
-	// const url = buildUrl()
-	var requestOptions = {
-	  method: 'POST',
-	  redirect: 'follow'
+	const encrypted = encrypt(secret, passphrase);
+	const timeOfExpiry = getExpiry(expiry, expiryUnit);
+
+	const body = {
+		"secret": encrypted,
+		"limit": accessesLimit,
+		"expiry": timeOfExpiry
 	};
 
-	fetch("https://us-central1-secret-messages-7749d.cloudfunctions.net/create?secret=h2i&limit=1", postRequestOptions)
+	const options = {
+		method: 'POST',
+		redirect: 'follow',
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: JSON.stringify(body)
+	};
+
+	fetch("/create", options)
 	  .then(function(response) {
 	  	return response.text();
 	  })
@@ -47,17 +68,21 @@ function encrypt(secret, passphrase) {
 	return secret;
 }
 
-function buildUrl(keyValues, action) {
-	let url = `${baseUrl}/${action}?`;
-
-	if (keyValues.size == 0) {
-		return url;
-	}
-
-	for (let [key, val] of keyValues) {
-		url = `${url}${key}=${value}&`
-	}
-
-	url = url.slice(0, -1); // remove trailing &
-	return url;
+function getExpiry(expiry, expiryUnit) {
+	return 1000*1000;
 }
+
+// function buildUrl(keyValues, action) {
+// 	let url = `${baseUrl}/${action}?`;
+
+// 	if (keyValues.size == 0) {
+// 		return url;
+// 	}
+
+// 	for (let [key, val] of keyValues) {
+// 		url = `${url}${key}=${value}&`
+// 	}
+
+// 	url = url.slice(0, -1); // remove trailing &
+// 	return url;
+// }
