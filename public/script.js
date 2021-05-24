002d3eebcafb394b9e4e635ecaab23ca85fcaeaa
unknown <wong.steven604@gmail.com>
2021-05-23 19:14:01 -0700

    clear input data on click

--- public/script.js
+++ public/script.js
@@ -13,6 +13,7 @@ function handleFormSubmit(event) {
 	const form = event.currentTarget;
 	const formData = Object.fromEntries(new FormData(form).entries());
 
+
 	createSecret(
 		formData.secret, 
 		formData.limit, 
@@ -20,6 +21,13 @@ function handleFormSubmit(event) {
 		formData.expiryUnit, 
 		formData.passphrase
 	);
+}
+
+function clearDataOnClick() {
+	document.getElementById("secret").value="";
+	document.getElementById("passphrase").value="";
+	document.getElementById("limit").value=null;
+	document.getElementById("expiry").value=null;
 }
 
 
