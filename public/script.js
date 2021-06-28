cfd4201d15707c1b4e60289c721eb2002bc25cda
steven wong <wong.steven604@gmail.com>
2021-06-28 09:28:22 -0700

    ui enhancements

--- public/script.js
+++ public/script.js
@@ -2,130 +2,155 @@ var formdata = new FormData();
 
 // const baseUrl = "https://us-central1-secret-messages-7749d.cloudfunctions.net/"; //create?secret=hi&limit=1"
 
-
-const DEFAULT_EXPIRY = 60*60; // one hour
+const DEFAULT_EXPIRY = 60 * 60; // one hour
 
 const createForm = document.getElementById("create-form");
 createForm.addEventListener("submit", handleFormSubmit);
+
+const retrieveSecretForm = document.getElementById("retrieve-secret-form");
+retrieveSecretForm.addEventListener("submit", handleRetrieveSubmit);
 
 const modal = document.getElementById("myModal");
 const span = document.getElementsByClassName("close")[0];
 const fqdn = window.location.hostname;
 
 function handleFormSubmit(event) {
-	event.preventDefault();
-	const form = event.currentTarget;
-	const formData = Object.fromEntries(new FormData(form).entries());
+  event.preventDefault();
+  const form = event.currentTarget;
+  const formData = Object.fromEntries(new FormData(form).entries());
 
-	clearDataOnClick();
+  createShareableLink("123");  
+  clearDataOnClick();
 
-	createSecret(
-		formData.secret, 
-		formData.limit, 
-		formData.expiresIn, 
-		formData.expiryUnit, 
-		formData.passphrase
-	);
+  createSecret(
+    formData.secret,
+    formData.limit,
+    formData.expiresIn,
+    formData.expiryUnit,
+    formData.passphrase
+  );
 }
 
-function clearDataOnClick() {
-	document.getElementById("secret").value="";
-	document.getElementById("passphrase").value="";
-	document.getElementById("limit").value=null;
-	document.getElementById("expiresIn").value=null;
+function handleRetrieveSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const formData = Object.fromEntries(new FormData(form).entries());
+
+  clearDataOnClick();
+
+  let secretMessage = fetchSecret(
+    // formData.passphrase,
+    formData.secretid
+  );
+
+  console.log("see if code goes here!");
+  console.log(secretMessage);
 }
 
-function createSecret(secret, accessesLimit, expiresIn, expiryUnit, passphrase) {
-	const encrypted = encrypt(secret, passphrase);
-	const expiresInSeconds = getExpiryInSeconds(expiresIn, expiryUnit);
+function createSecret(
+  secret,
+  accessesLimit,
+  expiresIn,
+  expiryUnit,
+  passphrase
+) {
+  const encrypted = encrypt(secret, passphrase);
+  const expiresInSeconds = getExpiryInSeconds(expiresIn, expiryUnit);
 
-	const body = {
-		"secret": encrypted,
-		"limit": accessesLimit,
-		"expiresIn": expiresInSeconds
-	};
+  const body = {
+    secret: encrypted,
+    limit: accessesLimit,
+    expiresIn: expiresInSeconds,
+  };
 
-	const options = {
-		method: 'POST',
-		redirect: 'follow',
-		headers: {
-			"Content-Type": "application/json",
-			"Accept": "application/json"
-		},
-		body: JSON.stringify(body)
-	};
+  const options = {
+    method: "POST",
+    redirect: "follow",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(body),
+  };
 
-	return fetch("/create", options)
-	  .then(response => response.text())
-	  .then(function(result) {
-	  	console.log(result);
-		createShareableLink(result);
-	  	return result;
-	  }) 
-	  .catch(function(error) {
-	  	console.log('error', error);
-	  });
+  return fetch("/create", options)
+    .then((response) => response.text())
+    .then(function (result) {
+      console.log(result);
+      createShareableLink(result);
+      return result;
+    })
+    .catch(function (error) {
+      console.log("error", error);
+    });
 }
 
-
-
-function createShareableLink(json) {
-	let obj = JSON.parse(json);
-	let id = obj.id;
-	let url = buildFetchUrl(id);
-	displayShareableLink(url);
+function createShareableLink(id) {
+  // let obj = JSON.parse(json);
+  // let id = obj.id;
+  let url = buildFetchUrl(id);
+  displayShareableLink(url);
 }
+
 
 function buildFetchUrl(id) {
 	return fqdn + "/fetch?id=" + id;
 }
 
 function deleteSecret(id) {
-	// TODO
+  // TODO
 }
 
 function fetchSecret(id) {
-	return fetch("/fetch?id=" + id)
-		.then(response => response.json())
-		.then(function(data) {
-			console.log(data);
-			return data;
-		});
+  return fetch("/fetch?id=" + id)
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data);
+      return data;
+    });
 }
 
 function encrypt(secret, passphrase) {
-	// TODO
-	return secret;
+  // TODO
+  return secret;
 }
 
 function getExpiryInSeconds(expiry, expiryUnit) {
-	if (!expiry) {
-		return DEFAULT_EXPIRY;
-	}
+  if (!expiry) {
+    return DEFAULT_EXPIRY;
+  }
 
-	switch(expiryUnit) {
-	  case "minutes":
-	    return expiry * 60;
-	  case "hours":
-	    return expiry * 60 * 60;
-	  case "days":
-	    return expiry * 24 * 60 * 60;
-	  default:
-	    return DEFAULT_EXPIRY;
-	}
+  switch (expiryUnit) {
+    case "minutes":
+      return expiry * 60;
+    case "hours":
+      return expiry * 60 * 60;
+    case "days":
+      return 24 * 60 * 60;
+    default:
+      return DEFAULT_EXPIRY;
+  }
+}
+
+function clearDataOnClick() {
+  document.getElementById("secret").value = "";
+  document.getElementById("passphrase").value = "";
+  document.getElementById("secretid").value = "";
+  document.getElementById("limit").value = null;
+  document.getElementById("expiresIn").value = null;
 }
 
 function displayShareableLink(url) {
-	document.getElementById("modal-paragraph").innerHTML = "Secret id: " + url;
-	modal.style.display = "block";
+  document.getElementById("modal-paragraph").innerHTML = "Secret id: " + url;
+  modal.style.display = "block";
 }
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
