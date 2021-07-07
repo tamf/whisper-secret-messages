00a7849fa3739bfc28bf171e3f84189f61d8dba6
steven wong <wong.steven604@gmail.com>
2021-07-07 12:11:50 -0700

    shareable url fits in modal and add copy to clipboard functionality

--- public/script.js
+++ public/script.js
@@ -34,6 +34,12 @@ if (retrieveSecretForm) {
   retrieveSecretForm.addEventListener("submit", handleRetrieveSubmit);
 }
 
+const copy = document.getElementById("copy-button");
+
+if (copy) {
+  copy.addEventListener("click", copyToClipBoard);
+}
+
 const span = document.getElementsByClassName("close")[0];
 const fqdn = window.location.host;
 
@@ -98,14 +104,14 @@ function handleRetrieveSubmit(event) {
 
   clearDataOnClick();
 
-  fetchSecret(
-    formData.secretid
-  ).then((result) => {
-    return decrypt(result.secret, formData.passphrase);
-  }).then((decrypted) => {
-    console.log(decrypted);
-    document.getElementById("secret-message-box").innerHTML = decrypted;
-  });
+  fetchSecret(formData.secretid)
+    .then((result) => {
+      return decrypt(result.secret, formData.passphrase);
+    })
+    .then((decrypted) => {
+      console.log(decrypted);
+      document.getElementById("secret-message-box").innerHTML = decrypted;
+    });
 }
 
 function createShareableLink(json, passphrase) {
@@ -115,7 +121,13 @@ function createShareableLink(json, passphrase) {
 }
 
 function buildFetchUrl(id, passphrase) {
-  return fqdn + "/retrieve-secret?id=" + encodeURIComponent(id) + "&passphrase=" + encodeURIComponent(passphrase);
+  return (
+    fqdn +
+    "/retrieve-secret?id=" +
+    encodeURIComponent(id) +
+    "&passphrase=" +
+    encodeURIComponent(passphrase)
+  );
 }
 
 function deleteSecret(id) {
@@ -179,7 +191,7 @@ async function encrypt(msg, passphrase) {
 
   const output = {
     cipherText: btoa(String.fromCharCode.apply(null, cipher)),
-    passphrase: passphraseGiven ? "" : passphrase
+    passphrase: passphraseGiven ? "" : passphrase,
   };
 
   return output;
@@ -199,36 +211,36 @@ async function decrypt(cipherText, passphrase) {
   const encrypted = cipher.slice(IV_LEN + SALT_LEN);
 
   const keyMaterial = await window.crypto.subtle.importKey(
-      "raw",
-      enc.encode(passphrase),
-      "PBKDF2",
-      false,
-      ["deriveBits", "deriveKey"]
+    "raw",
+    enc.encode(passphrase),
+    "PBKDF2",
+    false,
+    ["deriveBits", "deriveKey"]
   );
 
   const key = await window.crypto.subtle.deriveKey(
-      {
-        name: "PBKDF2",
-        salt: salt,
-        iterations: 100000,
-        hash: "SHA-256",
-      },
-      keyMaterial,
-      { name: "AES-CBC", length: 256 },
-      true,
-      ["encrypt", "decrypt"]
+    {
+      name: "PBKDF2",
+      salt: salt,
+      iterations: 100000,
+      hash: "SHA-256",
+    },
+    keyMaterial,
+    { name: "AES-CBC", length: 256 },
+    true,
+    ["encrypt", "decrypt"]
   );
 
   const decrypted = await window.crypto.subtle.decrypt(
-      {
-        name: "AES-CBC",
-        iv: iv,
-      },
-      key,
-      encrypted
+    {
+      name: "AES-CBC",
+      iv: iv,
+    },
+    key,
+    encrypted
   );
 
-   return dec.decode(decrypted);
+  return dec.decode(decrypted);
 }
 
 function getExpiryInSeconds(expiry, expiryUnit) {
@@ -276,6 +288,19 @@ function clearDataOnClick() {
   }
 }
 
+function copyToClipBoard() {
+  let text = document.getElementById("modal-paragraph").firstChild.data;
+  console.log(text);
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      alert("Copied!");
+    })
+    .catch((error) => {
+      alert("Copy failed!");
+    });
+}
+
 function displayShareableLink(url) {
-  document.getElementById("modal-paragraph").innerHTML = "  Sharing link: " + url;
+  document.getElementById("modal-paragraph").innerHTML = url;
 }
